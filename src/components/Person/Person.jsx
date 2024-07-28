93eee0520a3c7a0820d4569281d80f76c7cd82f7
Ivan Opar <vanya.opar@gmail.com>
2024-07-28 16:42:24 +0300

    person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnersText = 'I am not married';
+
+  if (isMarried === true && sex === 'm') {
+    partnersText = `${partnerName} is my wife`;
+  } else if (isMarried === true && sex === 'f') {
+    partnersText = `${partnerName} is my husband`;
+  } else {
+    partnersText = 'I am not married';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnersText}</p>
+    </section>
+  );
+};
